cd1a89e9076884e4b20966ed8be1839b567d3dbc
Bhushan Mokal <bhushanmokal02@gmail.com>
2023-05-09 18:24:00 +0530

    Initial Commit

---
+++ ListNavigationsDemo/ListNavigationsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListNavigationsDemoApp.swift
+//  ListNavigationsDemo
+//
+//  Created by Bhushan Mokal on 09/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ListNavigationsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
